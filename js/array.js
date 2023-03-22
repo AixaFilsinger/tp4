19c4388c3457bbc9f3754fde0bccb69cbc126b9c
Aixa Filsinger <macafil99@gmail.com>
2023-03-22 04:27:43 -0300

    Se realizo el ejercicio array de objetos lista producto

---
+++ js/array.js
@@ -0,0 +1,110 @@
+let listaProductos = [
+  {
+    nombreProducto: "Hidratante facial",
+    precio: 3500,
+    categoria: "Hidratante",
+  },
+  { nombreProducto: "Tonico exfoliante", precio: 2350.45, categoria: "Tonico" },
+  {
+    nombreProducto: "Protector solar factor 50",
+    precio: 3300,
+    categoria: "Protector solar",
+  },
+  {
+    nombreProducto: "Protector solar toque seco factor 50",
+    precio: 4100,
+    categoria: "Protector solar",
+  },
+  {
+    nombreProducto: "Protector solar con color factor 50",
+    precio: 3850.5,
+    categoria: "Protector solar",
+  },
+  {
+    nombreProducto: "Gel de limpieza facial",
+    precio: 1740.99,
+    categoria: "Limpieza",
+  },
+  { nombreProducto: "Tonico hidratante", precio: 1250.99, categoria: "Tonico" },
+  { nombreProducto: "Sérum hidratante", precio: 4250.99, categoria: "Sérum" },
+  {
+    nombreProducto: "Exfoliante de azúcar cherry",
+    precio: 1200,
+    categoria: "Labios",
+  },
+  {
+    nombreProducto: "Pads de hidrogel para contorno de ojos",
+    precio: 2800,
+    categoria: "Ojos",
+  },
+  {
+    nombreProducto: "Mascarilla facial",
+    precio: 3250.99,
+    categoria: "Mascaras",
+  },
+  { nombreProducto: "Sérum facial con PHA", precio: 3200, categoria: "Sérum" },
+  { nombreProducto: "Bálsamo labial", precio: 3200, categoria: "Labios" },
+  { nombreProducto: "Crema contorno de ojos", precio: 3200, categoria: "Ojos" },
+  {
+    nombreProducto: "Protector solar en barra",
+    precio: 3800,
+    categoria: "Protector solar",
+  },
+  {
+    nombreProducto: "Mascara de hidratación y reparación",
+    precio: 1200,
+    categoria: "Mascaras",
+  },
+  { nombreProducto: "Agua micellar", precio: 2890, categoria: "Limpieza" },
+];
+
+
+function Tabla(listaproducto) {
+  
+  document.write(`<table>
+                <tbody>`);
+  document.write(`<th>Producto</th>`);
+  document.write(`<th>Precio</th>`);
+  document.write(`<th>Categoria</th>`);
+
+  for (let i = 0; i < listaproducto.length; i++) {
+    document.write("<tr>");
+    document.write(`<td style="padding: 12px;">${listaproducto[i].nombreProducto}</td>`);
+    document.write(`<td style="padding: 12px;">${listaproducto[i].precio}</td>`);
+    document.write(`<td style="padding: 12px;">${listaproducto[i].categoria}</td>`);
+    document.write("</tr>");
+  }
+
+  document.write(`    </tbody>
+                    </table>`);
+  document.write("</section>");
+}
+
+document.write(`<h1>Lista de productos</h1>`);
+Tabla(listaProductos);
+document.write("<hr>")
+document.write("<h2>Filtrar  protectores solares</h2>");
+let filtroProtectorSolar = listaProductos.filter((producto) =>
+  producto.categoria.includes("Protector solar")
+);
+document.write("<hr>")
+Tabla(filtroProtectorSolar);
+document.write("<hr>")
+document.write("<h2>Busqueda de producto Sérum</h2>");
+document.write("<hr>")
+let buscarProductoSerum = [
+  listaProductos.find((producto) => producto.categoria.includes("Sérum")),
+];
+
+Tabla(buscarProductoSerum);
+document.write("<hr>")
+document.write("<h2>Busqueda de producto Bruma</h2>");
+document.write("<hr>")
+let buscarProductoBruma = [
+  listaProductos.find((producto) => producto.categoria.includes("Bruma")),
+];
+if (buscarProductoBruma[0] === undefined) {
+  document.write("<h3>No se encontro el producto de categoria: Bruma</h3>");
+} else {
+  Tabla(buscarProductoBruma);
+}
